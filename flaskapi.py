b1b2da50133e3596ad94c9472fa8b64e32d0143e
SuperArcherG <judahbertou@gmail.com>
2022-12-27 22:08:38 -0500

    Updates

--- flaskapi.py
+++ flaskapi.py
@@ -1,11 +1,49 @@
-from flask import Flask, json, send_file, request
+from flask import Flask, flash, json, send_file, request, redirect, url_for
+from werkzeug.utils import secure_filename
 import os
+
+Prod = True
+
+UPLOAD_FOLDER = 'levels/uploads/'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'heic'}
 
 companies = [{"id": 1, "name": "Company One"}, {"id": 2, "name": "Company Two"}]
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route("/")
+def allowed_file(filename):
+ return '.' in filename and \
+ filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(request.url + "uploads?name=" + filename)
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+@app.route("/e")
 def hello():
  return "<h1>This is the server for the test level select screen!</h1>"
 
@@ -26,6 +64,11 @@ def icon():
  filename = request.args.get('id') + ".jpeg"
  return send_file('levels/icon/' + filename, mimetype='image/jpeg')
 
+@app.route("/uploads", methods=['GET','POST'])
+def uploads():
+ filename = request.args.get('name')
+ return send_file('levels/uploads/' + filename)
+
 @app.route("/info", methods=['GET','POST'])
 def info():
  filename = request.args.get('id') + ".json"
@@ -33,7 +76,8 @@ def info():
 
 
 if __name__ == '__main__':
- app.run(host='localhost', port=80, debug=True)
-
-
-
+ if Prod:
+  from waitress import serve
+  serve(app,host='192.168.0.123', port=80)
+ else:
+  app.Run(host = '192.168.0.123', port=80, debug=True)
