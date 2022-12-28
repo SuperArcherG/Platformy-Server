0233d4cd79de2cc2cb1eabbdf3bfcdf30ecfb873
SuperArcherG <JudahBertou@gmail.com>
2022-12-27 21:01:10 -0500

    RAwr

--- flaskapi.py
+++ flaskapi.py
@@ -24,11 +24,16 @@ def cover():
 @app.route("/icon", methods=['GET','POST'])
 def icon():
  filename = request.args.get('id') + ".jpeg"
- return send_file('levels/icon/'+filename, mimetype='image/jpeg')
+ return send_file('levels/icon/' + filename, mimetype='image/jpeg')
+
+@app.route("/info", methods=['GET','POST'])
+def info():
+ filename = request.args.get('id') + ".json"
+ return send_file("levels/info/" + filename)
+
 
 if __name__ == '__main__':
-
- app.run(host='192.168.0.123', port=80, debug=True)
+ app.run(host='localhost', port=80, debug=True)
 
 
 
